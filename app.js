cdd1b2abbb6389f668a74fdbc7ab91f7aa1e8018
jmbenavides <jorgeok.2000@gmail.com>
2018-05-21 12:04:04 -0500

    Trabajo
    lo que he hecho hasta ahora (12:01 12/05/2018)

---
+++ app.js
@@ -0,0 +1,69 @@
+ var express= require('express'); 
+ var mongoose= require('mongoose');
+ var bodyParser= require('body-parser');
+ var app= express();
+
+app.set("view engine","jade");
+
+mongoose.connect("mongodb://localhost/objetos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+var personaSchema={
+	nombre: String,
+	apellido: String,
+	email: String,
+	contrasena:String
+};
+
+var User=mongoose.model("",personaSchema);
+
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res ){
+	
+	/*var data={
+		user:"jmbenavides",
+		password:"123"
+	}
+
+	var persona= new User(data);
+
+	persona.save(function(err){
+		console.log(persona);
+	});*/
+	res.render("index");
+});
+
+app.post("/formulario",function(req,res){
+	console.log(req.body);
+
+	var data={
+		nombre:req.body.nombre,
+		apellido:req.body.apellido,
+		email:req.body.email,		
+		contrasena:req.body.contrasena
+	}
+
+	var user = new User(data);
+
+	user.save(function(err){
+		console.log(user);
+
+	});
+	res.render("formulario/registroexitoso");
+});
+
+app.get("/formulario/registro",function(req,res){
+	res.render("formulario/registro");
+});
+
+app.get("/formulario/iniciarsesion",function(req,res){
+	res.render("formulario/iniciarsesion");
+});
+
+app.listen(8080);
