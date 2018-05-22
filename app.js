f85ae1a5a7c16243bdf817011bd4632fa9d25372
jmbenavides <jorgeok.2000@gmail.com>
2018-05-22 17:02:45 -0500

    vr 3
    toma los datos de la persona que esta ingresando y los compara con los de la base de datos

--- app.js
+++ app.js
@@ -10,81 +10,80 @@ mongoose.connect("mongodb://localhost/objetos");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
 var personaSchema={
-	nombre: String,
+	nombre: String,	
 	apellido: String,
 	email: String,
-	contrasena:String
+	usuario: String,
+	contrasena:String,
+	tareas:[],
+	grupo: String
 };
 
 var User=mongoose.model("",personaSchema);
-
 app.use(express.static("public"));
-
-
 app.get("/",function(req,res ){
-	
-	/*var data={
-		user:"jmbenavides",
-		password:"123"
-	}
-
-	var persona= new User(data);
-
-	persona.save(function(err){
-		console.log(persona);
-	});*/
 	res.render("index");
 });
-
 app.post("/formulario",function(req,res){
 	console.log(req.body);
-
 	var data={
 		nombre:req.body.nombre,
 		apellido:req.body.apellido,
 		email:req.body.email,		
 		contrasena:req.body.contrasena,
-		usuario:req.body.usuario
+		usuario:req.body.usuario,
+		tareas:null,
+		grupo:"";
 	}
-	var user = new User(data);
-
+	var persona = new User(data);
 	user.save(function(err){
 		console.log(user);
 	});
-
 	res.render("formulario/registroexitoso");
 });
-
 app.get("/formulario/registro",function(req,res){
 	res.render("formulario/registro");
 });
-
-
 app.get("/formulario/iniciarsesion",function(req,res){
 	res.render("formulario/iniciarsesion");
 });
-
-app.post("/formulario/iniciarsesion",function(req,res){
-
+app.post("/cliente/index",function(req,res){
+	var sw= false,persona;
 	User.find(function(error,documento){
 		if (error) {
-			console.log(error);
+			console.log("error");
 		}
+		
+		documento.forEach(function(elemento, index, arreglo){
+			console.log(elemento.usuario);
+			console.log(elemento.email);
+			console.log(elemento.contrasena);
+
+			console.log(req.body.usuario);
+			console.log(req.body.contrasena);
 
 
-		res.render("/cliente/index",{
-			Users:documento,
-			usuario:req.body.usuario,			
-			contrasena:req.body.contrasena
-		})
+			console.log((elemento.usuario == req.body.usuario || elemento.email==req.body.usuario)&& 
+				elemento.contrasena == req.body.contrasena);
+			if((elemento.usuario == req.body.usuario || elemento.email==req.body.usuario)&& 
+				elemento.contrasena == req.body.contrasena){
+				
+				persona=elemento;
+				sw=true;
+			}
+			
+		});
+			if(sw=true){
+				console.log(persona+"");
+				res.render("cliente/index",{persona});
+			}else{
+				res.send("este usuario no existe");
+			}
+
 	});
-
-
-	res.render("formulario/iniciarsesion");
 });
+
 
 
 app.listen(8080);
