f2b46eab75e2eed6a0cd991af642512e886bd52a
jmbenavides <jorgeok.2000@gmail.com>
2018-05-27 18:39:38 -0500

    vr 9

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(method_override("_method"));
 app.set("view engine","jade");
 
-
-
-
-
-
 var personaSchema={
 	nombre: String,	
 	apellido: String,
@@ -49,7 +44,7 @@ app.post("/formulario",function(req,res){
 	}
 	var person = new User(data);
 	person.save(function(err){
-			console.log(person);
+		//	console.log(person);
 	});
 	res.render("formulario/registroexitoso");
 });
@@ -63,33 +58,27 @@ app.get("/formulario/iniciarsesion",function(req,res){
 });
 
 var persona;
+
 app.post("/cliente/index",function(req,res){
-	User.findOne({"usuario": req.body.usuario},function(error,person){
-		
+	User.findOne({"usuario": req.body.usuario},function(error,person){		
 		persona=person;
 		if(person != null){
 			res.render("cliente/index",{persona});	
 		}else{
 			res.send("este usuario no existe");	
-		}
-		
+		}		
 	})
 });
 
-app.get("/cliente/nuevoproyecto",function(req,res){
-	
+app.get("/cliente/nuevoproyecto",function(req,res){	
 	res.render("cliente/nuevoproyecto",{persona})
 });
 
-app.get("/cliente",function(req,res){
-	
+app.get("/cliente",function(req,res){	
 	res.render("cliente/index",{persona})
 });
 
 app.put("/cliente/:id",function(req,res){
-	
-	var x=[];
-
 	var proyect={
 		nombre:req.body.nombre,
 		master:persona.usuario,
@@ -98,22 +87,17 @@ app.put("/cliente/:id",function(req,res){
 		tareas:[]
 	};
 	persona.proyectos.push(proyect);
-		
 	var dat={
 		proyectos:persona.proyectos
 	}
 	User.update({"_id": req.params.id},dat,function(personay){
-		console.log(persona); 	
 		res.render("cliente/index",{persona});
-
 	});
-
 });
-
 
 app.get("/proyecto/:num",function(req,res){
 	var creador,descripcion,cols,tareas;
-	console.log(persona);
+	//console.log(persona);
 	persona.proyectos.forEach(function(item){
 		
 		if(item.nombre==req.params.num){
@@ -122,57 +106,120 @@ app.get("/proyecto/:num",function(req,res){
 			cols=item.colaboradores;
 			tareas=item.tareas;
 		}
-
-
 	});
 
-
-	res.render("cliente/proyecto",{nombre:req.params.num,persona,creador,descripcion,cols,tareas});
+	res.render("cliente/proyecto",{
+		nombre:req.params.num,
+		persona,
+		creador,
+		descripcion,
+		cols,
+		tareas});
 });
 
 app.get("/cliente/proyecto/nuevatarea/:num",function(req,res){
-
-	var cols,tareas;
-	console.log(persona);
+	var cols,tareas,nombre;
+	//console.log(persona);
 	persona.proyectos.forEach(function(item){
 		if(item.nombre==req.params.num){
 			cols=item.colaboradores;
 			tareas=item.tareas;
+			nombre=item.nombre;
 		}
 	});
-	res.render("cliente/nuevatarea",{cols,tareas});
-
-
-
+	res.render("cliente/nuevatarea",{nombre,cols,tareas});
 });
 
+app.put("/cliente/proyecto/nuevatarea/:num",function(req,res){
 
-//esta parte de aqui todavia no la usamos
-app.put("/cliente/:id",function(req,res){
-	
-	var x=[];
-
-	var proyect={
+	var tarea={
 		nombre:req.body.nombre,
-		master:persona.usuario,
-		descripcion:req.body.descripcion,
-		colaboradores:[],
-		tareas:[]
+		colaborador:req.body.col
 	};
-	persona.proyectos.push(proyect);
-		
+	var creador,descripcion,cols,tareas,proyect;
+	persona.proyectos.forEach(function(item){
+		if(item.nombre==req.params.num){	
+			item.tareas.push(tarea);
+			creador=item.master;
+			descripcion=item.descripcion;
+			cols=item.colaboradores;
+			tareas=item.tareas;
+			proyect=item;
+		}
+	});
 	var dat={
 		proyectos:persona.proyectos
-	}
-	User.update({"_id": req.params.id},dat,function(personay){
-		console.log(persona); 	
-		res.render("cliente/index",{persona});
-
+	}	
+	User.update({"_id": persona._id},dat,function(personay){
 	});
-
+	res.render("cliente/proyecto",{nombre:req.params.num,persona,creador,descripcion,cols,tareas});
 });
 
+app.get("/cliente/nuevocolaborador/:num",function(req,res){
+	
+	var creador;
+	persona.proyectos.forEach(function(item){
+		if(item.nombre==req.params.num){
+			cols=item.colaboradores;
+			tareas=item.tareas;
+			nombre=item.nombre;
+			creador=item.master;
+			
+		}
+	});
+	
+	User.find(function(error,personas){
+		res.render("cliente/nuevocolaborador",{nombre:req.params.num,personas,creador});		
+	});
+});
+
+app.put("/cliente/nuevocolaborador/:num",function(req,res){
+	
+	var creador,nombre,cols,tareas,proyect;
+	
+	var z,data,data2;
+	persona.proyectos.forEach(function(item){
+
+		if(item.nombre==req.params.num){
+			creador=item.master;
+			descripcion=item.descripcion;
+			tareas=item.tareas;
+			proyect=item;
+
+			User.findOne({"usuario": req.body.col},function(error,personax){		
+				z=personax;
+				console.log("ASDADADAD");
+				console.log(personax.proyectos);
+				console.log("diferencia");
+				
+				console.log(z.proyectos);	
+				var y=z.proyectos;
+				y.push(proyect);
+				z.proyectos=y;
 
 
+				data={
+					proyectos:z.proyectos
+				};
+
+				data2={
+					proyectos:persona.proyectos
+				};
+			});	
+		}
+	});
+
+	console.log("asdssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+	
+	console.log(proyect);
+	
+
+	User.update({"usuario":req.body.col.usuario},data,function(error,personax){
+	});
+
+	User.update({"usuario":persona.usuario},data2,function(error,personax){
+	});
+	res.render("cliente/proyecto",{nombre:req.params.num,persona,creador,descripcion,cols,tareas});
+});
 
 app.listen(8080);
