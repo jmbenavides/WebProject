cf9698e7515380ae45638c8f9c0e0877cb0c96be
jmbenavides <jorgeok.2000@gmail.com>
2018-05-30 10:57:54 -0500

    vr 10

--- app.js
+++ app.js
@@ -197,22 +197,19 @@ app.put("/cliente/nuevocolaborador/:num",function(req,res){
 				y.push(proyect);
 				z.proyectos=y;
 
-
+				persona.proyectos.push(proyect);
 				data={
 					proyectos:z.proyectos
 				};
-
 				data2={
 					proyectos:persona.proyectos
 				};
 			});	
+
+
 		}
 	});
-
-	console.log("asdssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
-	
 	console.log(proyect);
-	
 
 	User.update({"usuario":req.body.col.usuario},data,function(error,personax){
 	});
@@ -223,3 +220,5 @@ app.put("/cliente/nuevocolaborador/:num",function(req,res){
 });
 
 app.listen(8080);
+
+
